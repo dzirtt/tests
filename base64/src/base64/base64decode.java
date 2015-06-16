b6a091cdb67d078d4008d5b8a0965e1db492a19b
dzirtt <EXIA@SK>
2015-06-16 10:18:31 +0200

    fix error with non base64 charachters add key -r, remove non base64 chars

--- base64/src/base64/base64decode.java
+++ base64/src/base64/base64decode.java
@@ -1,16 +1,40 @@
 package base64;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 
 public class base64decode {
+
 	
+	//parameters for work program
+	private static Path OUT;
+	private static Path IN;
+	private static decodeACTIONS ACTION;
+	private static boolean REMOVE = false;
+
+	private enum decodeACTIONS {
+		DECODE, ENCODE
+	};
+
+	private static final char[] base64Chars = { 'A', 'B', 'C', 'D', 'E', 'F',
+			'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+			'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+			'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+			't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+			'6', '7', '8', '9', '+', '/' };
+
+	
+
 	public static void main(String[] args) {
 
 		// parse arg and init fields
@@ -32,10 +56,11 @@ public class base64decode {
 		} catch (IOException e) {
 			ShowErrorAndExit(e.getMessage());
 		}
-		
+
 	}
 
-	private static void convert() throws FileNotFoundException, IOException {
+	private static void convert() throws FileNotFoundException, IOException,
+			IllegalArgumentException {
 
 		BufferedInputStream inStream = new BufferedInputStream(
 				Files.newInputStream(IN, StandardOpenOption.READ));
@@ -48,6 +73,15 @@ public class base64decode {
 		inStream.close();
 
 		if (ACTION == decodeACTIONS.DECODE) {
+			byte[] nonBase = searchNonBase64Chars(tmp);
+			if (nonBase.length != 0) {
+				if (REMOVE) {
+					tmp = removeNonBase64Chars(tmp, nonBase);
+				} else {
+					ShowErrorAndExit("contains non Base64 characters\n Base64 chars is:"
+							+ Arrays.toString(base64Chars));
+				}
+			}
 			tmp = Base64.getDecoder().decode(tmp);
 		} else {
 			tmp = Base64.getEncoder().encode(tmp);
@@ -56,6 +90,49 @@ public class base64decode {
 		outStream.write(tmp);
 		outStream.flush();
 		outStream.close();
+
+	}
+
+	private static byte[] removeNonBase64Chars(byte[] from, byte[] what) {
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		boolean skip = false;
+
+		for (byte b_in : from) {
+			for (byte b2_ex : what) {
+				if (b_in == b2_ex) {
+					skip = true;
+					break;
+				}
+			}
+
+			if (!skip) 
+				out.write(b_in);
+			
+			skip = false;
+		}
+
+		return out.toByteArray();
+	}
+
+	private static byte[] searchNonBase64Chars(byte[] arr) {
+		ArrayList<Byte> base = new ArrayList<>();
+
+		for (char c : base64Chars) {
+			base.add((byte) c);
+		}
+
+		ByteArrayOutputStream tmp = new ByteArrayOutputStream();
+
+		// collect all non base54 chars
+		for (byte b : arr) {
+			if (!base.contains(b)) {
+				tmp.write(b);
+			}
+		}
+
+		return tmp.toByteArray();
 
 	}
 
@@ -82,6 +159,10 @@ public class base64decode {
 			case "e":
 				ACTION = decodeACTIONS.ENCODE;
 				break;
+			case "remove":
+			case "r":
+				REMOVE = true;
+				break;
 			default:
 				notRecognise += "|" + args[i];
 				break;
@@ -99,13 +180,4 @@ public class base64decode {
 		System.exit(-1);
 	}
 
-
-	private static Path OUT;
-	private static Path IN;
-	private static decodeACTIONS ACTION;
-
-	private enum decodeACTIONS {
-		DECODE, ENCODE
-	}
-			
 }
